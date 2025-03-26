c36687ea2d6cac2fd1b4e98cdfd43308d712ad4e
yigerenwan <raugenai@gmail.com>
2025-03-26 20:25:23 +0000

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ with open("requirements.txt", "r") as fp:
         if line[0] != "#":
             dep_packages.append(str(line.strip()))
 
-version = "0.0.2"
+version = "0.0.3"
 
 setup(
     name="raugen",
